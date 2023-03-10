4e03b72145ded55c8d3e32f70999f0954584d7c4
tejasribarla <tejasribarla188@gmail.com>
2023-03-10 05:48:08 +0000

    solution

---
+++ src/main/java/com/example/recipe/RecipeController.java
@@ -0,0 +1,42 @@
+/*
+ * 
+ * You can use the following import statements
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ * 
+ */
+
+// Write your code here
+
+package com.example.recipe;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.recipe.RecipeService;
+import java.util.*;
+
+@RestController 
+
+class RecipeController {
+    RecipeService recipeService=new RecipeService();
+
+    @GetMapping("/recipes")
+    public ArrayList<Recipe>getRecipes(){
+        return recipeService.getRecipes();
+    }
+    @GetMapping("/recipes/{recipeId}")
+    public Recipe getRecipeById(@PathVariable ("recipeId") int recipeId) {
+        return recipeService.getRecipeById(recipeId);
+    }
+    @PostMapping("/recipes")
+    public Recipe addRecipe(@RequestBody Recipe recipe){
+        return recipeService.addRecipe(recipe);
+    }
+    @PutMapping("/recipes/{recipeId}")
+    public Recipe updateRecipe(@PathVariable("recipeId") int recipeId,@RequestBody Recipe recipe) {
+        return recipeService.updateRecipe(recipeId,recipe);
+    }
+    @DeleteMapping("recipes/{recipeId}")
+    public void deleteRecipe(@PathVariable("recipeId") int recipeId) {
+        recipeService.deleteRecipe(recipeId);
+    }
+}
---
+++ src/main/java/com/example/recipe/RecipeRepository.java
@@ -0,0 +1,12 @@
+package com.example.recipe;
+
+import java.util.*;
+
+public interface RecipeRepository {
+    ArrayList<Recipe>getRecipes();
+    Recipe getRecipeById(int recipeId);
+    Recipe addRecipe(Recipe recipe);
+    Recipe updateRecipe(int recipeId,Recipe recipe);
+    void deleteRecipe(int recipeId);
+    
+}
